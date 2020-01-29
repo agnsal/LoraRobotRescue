b66dac5222002e31986949027ef940be8a0bde8a
Agnese Salutari <agneses92@hotmail.it>
2020-01-29 16:14:26 +0100

    Update LoraRobotRescue.ino

--- LoraRobotRescue/LoraRobotRescue.ino
+++ LoraRobotRescue/LoraRobotRescue.ino
@@ -1,6 +1,6 @@
 
 /*
-Copyright 2019 Agnese Salutari.
+Copyright 2019-2020 Agnese Salutari.
 Licensed under the Apache License, Version 2.0 (the "License"); 
 you may not use this file except in compliance with the License. 
 You may obtain a copy of the License at
